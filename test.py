057cf2bd33b65d0219671c78715a48581b426cb0
Erik Nomitch <enomitch@gmail.com>
2021-09-28 16:39:15 -0500

    Fix pipenv; add process_prompt and JSON

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import os
+import json
+import sys
 import openai
+from pprint import pprint
+
+from ipdb import set_trace
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,7 +18,7 @@ openai.api_key = OPENAI_API_KEY
 # - davinci-instruct-beta
 engines = openai.Engine.list()
 
-params = dict(
+working_params = dict(
     engine="davinci-instruct-beta",
     max_tokens=100,
     temperature=0.5,
@@ -25,10 +30,15 @@ params = dict(
     # logit_bias=None
 )
 
+def process_prompt(params, prompt):
+    completion = openai.Completion.create(**{**params, 'prompt': prompt})
+    response = completion.choices[0].text.lstrip()
 
-ENGINE = "davinci-instruct-beta"
-MAX_TOKENS = 100
-TEMPERATURE = 0.5
+    print(f"**{prompt}**")
+    print(f"> {response}")
+
+    return {**params, 'prompt': 'prompt', 'response': response}
+
 
 
 # PROMPT = """Explain the moon landing to a 6 year old in a few sentences."""
@@ -40,22 +50,11 @@ TEMPERATURE = 0.5
 # PROMPT = "What's the answer to this riddle: What language does a billboard speak?"
 # PROMPT = "Can God create a boulder so heavy that even he cannot lift it?"
 # PROMPT = "Have you ever had a dream that you, um, you had, your, you- you could, you’ll do, you- you wants, you, you could do so, you- you’ll do, you could- you, you want, you want them to do you so much you could do anything?"
+# PROMPT = "Does a photon experience time? If not, why?"
 PROMPT = "Does a photon experience time? If not, why?"
 
-# create a completion
-completion = openai.Completion.create(
-    engine=ENGINE,
-    prompt=PROMPT,
-    max_tokens=MAX_TOKENS,
-    temperature=TEMPERATURE,
-    top_p=1,
-    # stop=[],
-    presence_penalty=0,
-    frequency_penalty=0.4,
-    best_of=1
-    # logit_bias=None
-)
 
+output = process_prompt(working_params, PROMPT)
 
-print(f"**{PROMPT}**")
-print(f"> {completion.choices[0].text.lstrip()}")
+pprint(output)
+
